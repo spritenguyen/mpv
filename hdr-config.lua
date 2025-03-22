2e771cf44607dfc571677d5dbda1428ec7279728
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-03-23 02:39:42 +0700

    Update hdr-config.lua

--- hdr-config.lua
+++ hdr-config.lua
@@ -1,5 +1,20 @@
 mp.register_event("file-loaded", function()
-    -- Nhận diện thiết bị: PC hoặc Android
+    -- Hàm áp dụng cấu hình
+    local function apply_config(hwdec, vo, scale, dscale, tone_mapping, hdr_compute_peak, message)
+        mp.set_property("hwdec", hwdec)
+        mp.set_property("vo", vo)
+        mp.set_property("scale", scale)
+        mp.set_property("dscale", dscale)
+        if tone_mapping then
+            mp.set_property("tone-mapping", tone_mapping)
+        end
+        if hdr_compute_peak then
+            mp.set_property("hdr-compute-peak", hdr_compute_peak)
+        end
+        mp.osd_message(message, 6)
+    end
+
+    -- Nhận diện thiết bị và thông tin video
     local os_name = mp.get_property("os-name", "unknown")
     local gpu_context = mp.get_property("gpu-context", "unknown")
     local filename = mp.get_property("filename", "")
@@ -8,53 +23,20 @@ mp.register_event("file-loaded", function()
     local hdr_peak = mp.get_property("video-params/hdr-peak", 0)
     local codec = mp.get_property("video-codec", "unknown")
 
-    -- Trường hợp video từ YouTube, sử dụng cấu hình mặc định mpv.conf
-    --local demuxer = mp.get_property("demuxer", "unknown")
-   -- if demuxer == "lavf" and (filename:find("youtube.com") or filename:find("youtu.be")) then
-      --  mp.osd_message("YouTube video detected. Using default mpv.conf.", 6)
-       -- return
-    --end
+    -- Kiểm tra video có HDR hay không
+    local is_hdr = (hdr_format == "bt.2020" or color_space == "hdr" or tonumber(hdr_peak) > 0 or codec:find("hevc") or filename:find("HDR") or filename:find("hdr"))
 
-    -- Kiểm tra HDR dựa trên metadata và tên file
-    if hdr_format == "bt.2020" or color_space == "hdr" or tonumber(hdr_peak) > 0 or codec:find("hevc") or filename:find("HDR") or filename:find("hdr") then
-        -- Áp dụng cấu hình HDR cho thiết bị Android
+    if is_hdr then
         if os_name == "android" then
-            mp.set_property("hwdec", "mediacodec")
-            mp.set_property("vo", "gpu")
-            mp.set_property("scale", "bilinear")
-            mp.set_property("dscale", "mitchell")
-            mp.set_property("tone-mapping", "linear")
-            mp.set_property("hdr-compute-peak", "no")
-            mp.osd_message("HDR Configuration Applied for Android!", 6)
+            apply_config("mediacodec", "gpu", "bilinear", "mitchell", "linear", "no", "HDR Configuration Applied for Android!")
         else
-            -- Áp dụng cấu hình HDR cho PC
-            mp.set_property("hwdec", "auto-safe")
-            mp.set_property("vo", "gpu")
-            mp.set_property("scale", "ewa_lanczos")
-            mp.set_property("dscale", "mitchell")
-            mp.set_property("tone-mapping", "hable")
-            mp.set_property("hdr-compute-peak", "yes")
-            mp.osd_message("HDR Configuration Applied for PC!", 6)
+            apply_config("auto-safe", "gpu", "ewa_lanczos", "mitchell", "hable", "yes", "HDR Configuration Applied for PC!")
         end
     else
-        -- Áp dụng cấu hình đơn giản cho Non-HDR
         if os_name == "android" then
-            mp.set_property("hwdec", "mediacodec")
-            mp.set_property("vo", "gpu")
-            mp.set_property("scale", "bilinear")
-            mp.set_property("dscale", "bilinear")
-            mp.set_property("interpolation", "no")
-            mp.osd_message("Non-HDR Configuration Applied for Android!", 6)
+            apply_config("mediacodec", "gpu", "bilinear", "bilinear", nil, nil, "Non-HDR Configuration Applied for Android!")
         else
-            -- Cấu hình Non-HDR cho PC
-            mp.set_property("hwdec", "auto-safe")
-            --mp.set_property("vo", "gpu")
-            mp.set_property("scale", "bilinear")
-            mp.set_property("dscale", "bilinear")
-            mp.set_property("interpolation", "no")
-            --mp.set_property("volume-max", "100")
-            --mp.set_property("volume", "80")
-            mp.osd_message("Non-HDR Configuration Applied for PC!", 6)
+            apply_config("auto-safe", "gpu", "bilinear", "bilinear", nil, nil, "Non-HDR Configuration Applied for PC!")
         end
     end
 end)
