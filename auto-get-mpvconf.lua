bfc5bbcdad204bab3537c79dd3d8dfb6192c27ad
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-03-22 23:44:54 +0700

    Update auto-get-mpvconf.lua

--- auto-get-mpvconf.lua
+++ auto-get-mpvconf.lua
@@ -1,10 +1,10 @@
 mp.register_event("file-loaded", function()
     local hdr_format = mp.get_property("video-params/primaries", "unknown")
     if hdr_format == "bt.2020" then
-        mp.commandv("load-config", "hdr-config-android.conf")
-        mp.osd_message("HDR detected: Applying HDR config!")
+        mp.commandv("load-config", "hdr-config.conf")
+        mp.osd_message("HDR Configuration Applied!")
     else
-        mp.commandv("load-config", "sdr-config-android.conf")
-        mp.osd_message("SDR detected: Applying SDR config!")
+        mp.commandv("load-config", "sdr-config.conf")
+        mp.osd_message("SDR Configuration Applied!")
     end
 end)
