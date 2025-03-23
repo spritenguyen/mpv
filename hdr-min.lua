e8dcb1fd9baa2e2ebc85c90079241e2bd2d61b62
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-03-23 10:13:55 +0700

    Update hdr-min.lua

--- hdr-min.lua
+++ hdr-min.lua
@@ -1,24 +1,33 @@
 mp.register_event("file-loaded", function()
-    local file_size = tonumber(mp.get_property("file-size", "0")) -- Lấy kích thước file (tính bằng byte)
-    local duration = tonumber(mp.get_property("duration", "0")) -- Lấy thời lượng video (tính bằng giây)
-
-    if file_size > 0 and duration > 0 then
-        local bitrate = (file_size * 8) / (duration * 1000) -- Chuyển đổi từ byte sang kbps
-        if bitrate > 3000 then
-            mp.set_property("hwdec", "auto-safe")
-            mp.set_property("vo", "gpu-next")
-            mp.set_property("scale", "bicubic")
-            mp.set_property("tone-mapping", "mobius")
-            mp.set_property("hdr-compute-peak", "no")
-            mp.osd_message("Cấu hình 'bitrate cao' đã được áp dụng", 5)
-        else
-            mp.set_property("hwdec", "auto-safe")
-            mp.set_property("vo", "gpu")
-            mp.set_property("scale", "bilinear")
-            mp.set_property("interpolation", "no")
-            mp.osd_message("Cấu hình Non-HDR đã được áp dụng", 5)
-        end
+    local is_live = mp.get_property("stream-path", ""):find("http") ~= nil -- Phát hiện luồng trực tiếp
+    if is_live then
+        mp.set_property("hwdec", "auto-safe")
+        mp.set_property("vo", "gpu")
+        mp.set_property("scale", "bilinear")
+        mp.set_property("interpolation", "no")
+        mp.osd_message("Cấu hình 'Luồng trực tiếp' đã được áp dụng", 5)
     else
-        mp.osd_message("Không thể tính toán bitrate!", 5)
+        local file_size = tonumber(mp.get_property("file-size", "0")) -- Lấy kích thước file
+        local duration = tonumber(mp.get_property("duration", "0")) -- Lấy thời lượng video
+
+        if file_size > 0 and duration > 0 then
+            local bitrate = (file_size * 8) / (duration * 1000) -- Tính bitrate (kbps)
+            if bitrate > 3000 then
+                mp.set_property("hwdec", "auto-safe")
+                mp.set_property("vo", "gpu-next")
+                mp.set_property("scale", "bicubic")
+                mp.set_property("tone-mapping", "mobius")
+                mp.set_property("hdr-compute-peak", "no")
+                mp.osd_message("Cấu hình 'bitrate cao' đã được áp dụng", 5)
+            else
+                mp.set_property("hwdec", "auto-safe")
+                mp.set_property("vo", "gpu")
+                mp.set_property("scale", "bilinear")
+                mp.set_property("interpolation", "no")
+                mp.osd_message("Cấu hình Non-HDR đã được áp dụng", 5)
+            end
+        else
+            mp.osd_message("Không thể tính toán bitrate!", 5)
+        end
     end
 end)
