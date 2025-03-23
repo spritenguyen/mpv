f0a34f5fa61d0ad59800f5e9c074573f44fbc459
spritenguyen <120728603+spritenguyen@users.noreply.github.com>
2025-03-23 21:31:30 +0700

    Update tor.lua

--- tor.lua
+++ tor.lua
@@ -1,10 +1,15 @@
 -- Script: mpv_tor_proxy.lua
--- Mục đích: Kết nối MPV qua proxy Tor (127.0.0.1:9050)
+-- Mục đích: Kết nối MPV qua proxy Tor (127.0.0.1:9050) và hiển thị thông báo sau 6 giây
 
 local function set_tor_proxy()
     local proxy_address = "socks5://127.0.0.1:9050"
     mp.set_property("http-proxy", proxy_address)
     mp.msg.info("Proxy được thiết lập: " .. proxy_address)
+
+    -- Hiển thị thông báo sau 10 giây
+    mp.add_timeout(10, function()
+        mp.osd_message("Proxy được thiết lập: " .. proxy_address, 5) -- Hiển thị thông báo trong 5 giây
+    end)
 end
 
 mp.register_event("start-file", set_tor_proxy)
